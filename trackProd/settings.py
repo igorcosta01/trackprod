b49aad9b5d18a1844150fea80137bbad52cbe745
Igor Costa <igorfelipe.costa@hotmail.com>
2024-09-25 09:05:09 -0300

    Adicionando colunas qtd apontada e saldo

--- trackProd/settings.py
+++ trackProd/settings.py
@@ -108,11 +108,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = False
+USE_I18N = True
 
 USE_TZ = True
 
