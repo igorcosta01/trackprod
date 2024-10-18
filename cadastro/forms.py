5efb3c5e57e0e1d8e48545976a5457544cf5d5e9
Igor Costa <igorfelipe.costa@hotmail.com>
2024-10-17 22:15:39 -0300

    Adicionado Widgets no form Produto, fix form produto

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -4,14 +4,27 @@ from cadastro.models import Produto, UnidadeMedida, Material, Funcionario
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
-        fields = ['nome', 'codigo', 'descricao', 'material', 'peso', 'volume', 'cor']
+
+        widgets = {
+            'cliente': forms.Select(attrs={'class': 'form-control form-control-sm'}),  # Estilizando o select
+            'nome': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),  # Estilizando o select
+            'codigo': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),  # Estilizando o select
+            'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Descrição do Produto'}),
+            'material': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Material'}),
+            'peso': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'placeholder': 'Peso', 'min': '0'}),
+            'volume': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'placeholder': 'Volume', 'min': '0'}),
+            'cor': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Cor'}),
+        }
+
+        fields = ['cliente', 'nome', 'codigo', 'descricao', 'material', 'peso', 'volume', 'cor']
         labels = {
-            'nome': 'Nome',
-            'codigo': 'Código',
-            'descricao': 'Descrição',
+            'cliente': 'Cliente',
+            'nome': 'Nome Produto',
+            'codigo': 'Código (SKU)',
+            'descricao': 'Observação',
             'material': 'Material',
-            'peso': 'Peso',
-            'volume': 'Volume',
+            'peso': 'Peso (g)',
+            'volume': 'Volume (ml)',
             'cor': 'Cor'
         }
 
--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from producao import views as producao_views
 
 urlpatterns = [
+
+    path('new_ordem/', producao_views.nova_ordem_producao, name='new_ordem'),
 
     path('novo_produto/', views.novo_produto, name='novo_produto'),
     path('list_produtos/', views.list_produtos, name='list_produtos'),
